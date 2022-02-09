18ec96f8509380697ed9e7e24fbc9bfbfab7310a
Matthew Li <02matthewli@gmail.com>
2022-02-09 16:07:26 -0500

    Added: readme, updated: entity

---
+++ GuessMaster/GM_README.md
@@ -0,0 +1,11 @@
+Notes for my this project submission
+
+Things can be optimized better:
+
+1. The program works as is, but it could be tidied up with the addition of helper functions, which were not added in the interest of time.
+2. I should have used an array list which would have dynamic size, but per the assignment instructions I had to use a static array.
+
+I take full credit for the Entity.java, GuessMaster.java program whilst, the Date.java file was primarily written by ELEC 279 course staff with modification by me.
+
+
+MLi 2022-02-09
--- GuessMaster/GuessMaster.java
+++ GuessMaster/GuessMaster.java
@@ -1,15 +1,15 @@
 import java.util.Random; 
 
-
 public class GuessMaster {
     private int numberOfCandidateEntities;
-    private int szOfArr = 10;
+    private int szOfArr = 10;           // Setting the default array size to 10, change if you desire
     private Entity[] entities = new Entity[szOfArr];
 
     // class constructor
     public GuessMaster(){
         numberOfCandidateEntities = 0; // array needs to have a size
-        entities = new Entity[]{null, null, null, null, null, null, null, null, null, null};
+        entities = new Entity[]{null, null, null, null, null, null, null, null, null, null};    // need to initialize the array, this would have to change
+                                                                                                // depending on the array size, (NOT DYNAMIC)
     }
 
     public void addEntity(Entity entity){
@@ -18,7 +18,7 @@ public class GuessMaster {
     }
 
     public void playGame(Entity entity){
-        Date guess = new Date();
+        Date guess = new Date();    // creating new date object that will store guess made by user
         Boolean tryAgain = true;
         System.out.printf("Guess %s' birthday!\n", entity.getName());
         while(tryAgain){
@@ -38,24 +38,29 @@ public class GuessMaster {
             }
         }
         System.out.println();
-        playGame(genRandomEntityInd());
+        playGame(genRandomEntityInd()); // calling the game again (repeats until the user quits)
     }
 
+    /**
+     *  The conditional statement here catches all cases where the program tries to play the game with
+     *  an index that has been set to null (which would obviously not work).
+     *  Instead it randomly picks another index to play the game with. 
+     */
     public void playGame(int entityInd){
         if(entities[entityInd] == null){
             System.out.println("Sorry this field has not been intialized");
-            System.exit(-1);
+            playGame();
         }
         else{
             playGame(entities[entityInd]);
         }
-        
     }
 
     public void playGame(){
         playGame(genRandomEntityInd());
     }
 
+    // helper function 
     public int genRandomEntityInd(){
         Random rnd = new Random();
         int ind = rnd.nextInt(szOfArr);
@@ -70,22 +75,13 @@ public class GuessMaster {
         Entity usa = new Entity("United States", new Date("July", 4, 1776));
         Entity Matty = new Entity("Matthew Li", new Date("November", 15, 2002));
         Entity Canada = new Entity("Canada", new Date("July", 1, 1867));
-        Entity QU = new Entity("QU", new Date("December", 4, 1776));
-        Entity usa1 = new Entity("United States", new Date("September", 4, 1776));
-        Entity usa2 = new Entity("United States", new Date("June", 4, 1776));
-        Entity usa3 = new Entity("United States", new Date("August", 4, 1776));
-        Entity usa4 = new Entity("United States", new Date("May", 4, 1776));
-        
+       
         gm.addEntity(trudeau);
         gm.addEntity(dion);
         gm.addEntity(usa);
         gm.addEntity(Matty);
         gm.addEntity(Canada);
-        gm.addEntity(QU);
-        gm.addEntity(usa1);
-        gm.addEntity(usa2);
-        gm.addEntity(usa3);
-        gm.addEntity(usa4);
+       
 
         // game starts here
         gm.playGame();
